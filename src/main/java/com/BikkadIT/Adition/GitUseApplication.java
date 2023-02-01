b74c13e7b71883540ccd024f762c27f40aead6eb
anutashildar <annapurnapatil024@gmail.com>
2023-02-01 20:24:20 +0530

    changes by anu

---
+++ src/main/java/com/BikkadIT/Adition/GitUseApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.Adition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUseApplication.class, args);
+	}
+
+}
